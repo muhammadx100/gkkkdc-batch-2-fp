3e699150f260a3383b76b5bc0426d8bcc8d49b90
Irfanullah Khan <irfanullah.khan@jintech.com>
2024-11-05 16:06:04 +0500

    IK - 05/11/2024

---
+++ 5-data_types.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num = 10;
+	float f = 10.4544;
+	double d = 1009.8745373;
+	char c = 'A';
+	bool b = false; // false, 1, 0, 10, -2, NULL
+	cout<<b;
+}
---
+++ 6-operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	// operators: +, -, *, /, %
+	int n1 = 15, n2 = 15;
+	int result = 12;
+	
+	result = n1 + n2; 
+	cout<< result<<endl; 
+	result = n1 - n2;
+	cout<< result<<endl;
+	result = n1 * n2;
+	cout<< result<<endl;
+	result = n1 / n2;
+	cout<<result<<endl;
+	result = n1 % n2;
+	cout<<result<<endl;
+}
